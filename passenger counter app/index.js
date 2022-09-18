eb5898c6222600678a51b5ce600b6c8cd8ed4362
Anv3sh <anveshgreat11@gmail.com>
2022-09-19 00:50:03 +0530

    added functionality

---
+++ passenger counter app/index.js
@@ -0,0 +1,10 @@
+
+let countel = document.getElementById("count-el");
+let count = 0;
+function increment(){
+    countel.innerText=Number(countel.innerText)+1;
+}
+
+function decrement(){
+    countel.innerText=Number(countel.innerText)-1;
+}
